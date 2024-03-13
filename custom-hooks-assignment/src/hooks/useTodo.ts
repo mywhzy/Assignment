190e1454a9c074bfbd1079b9e3493dfcee251bf1
mywhzy <cmj2393a@naver.com>
2024-03-13 23:41:32 +0900

    feat: useTodo hook 구현

---
+++ custom-hooks-assignment/src/hooks/useTodo.ts
@@ -0,0 +1,30 @@
+import {useEffect, useState} from 'react';
+import axios, {AxiosError} from 'axios';
+
+export type dataType = {
+  userId: number,
+  id: number,
+  title: string,
+  completed: boolean,
+}
+
+export const useTodo = (id: number) => {
+  const [data, setData] = useState<undefined | dataType>(undefined);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<undefined | AxiosError>(undefined);
+
+  const url = process.env.REACT_APP_API_URL || '';
+
+  useEffect(() => {
+    axios.get(url + id)
+      .then(response => {
+        setData(response.data);
+        setIsLoading(!isLoading);
+      }).catch(error => {
+      setIsLoading(!isLoading);
+      setError(error);
+    });
+  }, []);
+
+  return {data, isLoading, error};
+};

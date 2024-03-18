62da45ad8a2856b4467c2b4e46facabd7d49d79b
mywhzy <cmj2393a@naver.com>
2024-03-18 19:05:17 +0900

    feat: ListItem Detail 페이지 구현

---
+++ custom-hooks-assignment/src/pages/ItemDetailPage.tsx
@@ -0,0 +1,30 @@
+import { useTodo } from '../hooks/useTodo';
+import { useParams } from 'react-router-dom';
+import { styled } from 'styled-components';
+import { ItemDetail } from '../components/ItemDetail';
+
+export const ItemDetailPage = () => {
+  const id = Number(useParams().id);
+  const { data, isLoading, error } = useTodo(id);
+
+  if (isLoading) return <h4>로딩 중...</h4>;
+  if (error) return <ErrorText>Error! id에 맞는 데이터를 불러올 수 없습니다.</ErrorText>;
+ 
+  return (
+    <Container>
+      <ItemDetail userId={data?.userId} id={data?.id} title={data?.title} completed={data?.completed} />
+    </Container>
+  );
+};
+
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 50px;
+`;
+
+const ErrorText = styled.h3`
+    color: palevioletred;
+    font-style: italic;
+`;

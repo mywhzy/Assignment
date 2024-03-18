49d1605084c2abf95d663067934febe952f82801
mywhzy <cmj2393a@naver.com>
2024-03-18 19:03:00 +0900

    feat: react-router-dom 적용

--- custom-hooks-assignment/src/App.tsx
+++ custom-hooks-assignment/src/App.tsx
@@ -1,6 +1,14 @@
+import { TodoListPage } from './pages/TodoListPage';
+import { Routes, Route } from 'react-router-dom';
+import { ItemDetailPage } from './pages/ItemDetailPage';
+
 function App() {
+
   return (
-    <></>
+    <Routes>
+      <Route index path="/" element={<TodoListPage />} />
+      <Route path="todos/:id" element={<ItemDetailPage />} />
+    </Routes>
   );
 }
 

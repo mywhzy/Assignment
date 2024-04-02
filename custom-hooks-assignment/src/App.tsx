22bcb7200f575683242e1fb9665db8842f8ab652
mywhzy <cmj2393a@naver.com>
2024-04-02 21:52:58 +0900

    feat: NotFoundPage Route 연결

--- custom-hooks-assignment/src/App.tsx
+++ custom-hooks-assignment/src/App.tsx
@@ -1,6 +1,7 @@
 import { TodoListPage } from './pages/TodoListPage';
 import { Routes, Route } from 'react-router-dom';
 import { TodoDetailPage } from './pages/TodoDetailPage';
+import { NotFoundPage } from './pages/NotFoundPage';
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <Routes>
       <Route index path="/" element={<TodoListPage />} />
       <Route path="todos/:id" element={<TodoDetailPage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 }

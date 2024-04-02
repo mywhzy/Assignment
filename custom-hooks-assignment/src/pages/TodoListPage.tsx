e192853540b32df1cd1c70263442505aa2712602
mywhzy <cmj2393a@naver.com>
2024-04-02 21:11:31 +0900

    fix: TodoListPage error 렌더링 조건 변경

--- custom-hooks-assignment/src/pages/TodoListPage.tsx
+++ custom-hooks-assignment/src/pages/TodoListPage.tsx
@@ -8,8 +8,8 @@ export const TodoListPage = () => {
   const navigate = useNavigate();
   const { data, isLoading, error } = useTodos();
 
-  if (!data || isLoading) return <h4>로딩 중...</h4>;
-  if (error) return <ErrorText>Error! 데이터를 받아오는 과정에서 Error가 발생했습니다.</ErrorText>;
+  if (isLoading) return <h4>로딩 중...</h4>;
+  if (!data || error) return <ErrorText>Error! 데이터를 받아오는 과정에서 Error가 발생했습니다.</ErrorText>;
 
   return (
     <Container>
